cddfa700fd6f22bb0ea9f0a4c1f4566b69494748
Aaron Bentley <abentley@panoramicfeedback.com>
2005-03-09 20:03:07 +0000

    added bugs, Switched to using uuid prefixes to ensure uniqueness

--- libbe/cmdutil.py
+++ libbe/cmdutil.py
@@ -1,15 +1,28 @@
 def unique_name(bug, bugs):
-    return bug.name
+    chars = 1
+    for some_bug in bugs:
+        if bug.uuid == some_bug.uuid:
+            continue
+        while (bug.uuid[:chars] == some_bug.uuid[:chars]):
+            chars+=1
+    return bug.uuid[:chars]
 
 class UserError(Exception):
     def __init__(self, msg):
         Exception.__init__(self, msg)
 
 def get_bug(spec, bug_dir):
+    matches = []
     bugs = list(bug_dir.list())
     for bug in bugs:
-        if bug.uuid == spec:
-            return bug
+        if bug.uuid.startswith(spec):
+            matches.append(bug)
+    if len(matches) > 1:
+        raise UserError("More than one bug matches %s.  Please be more"
+                        " specific." % spec)
+    if len(matches) == 1:
+        return matches[0]
+        
     matches = []
     for bug in bugs:
         if bug.name == spec:
