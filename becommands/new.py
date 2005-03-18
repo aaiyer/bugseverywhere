e0f027bc4d96afda56dcbf2ce3998a5bc9d88ff9
Aaron Bentley <aaron.bentley@utoronto.ca>
2005-03-18 01:22:07 +0000

    Made new print the bug ID of the new bug

--- becommands/new.py
+++ becommands/new.py
@@ -4,11 +4,12 @@ def execute(args):
     if len(args) != 1:
         raise cmdutil.UserError("Please supply a summary message")
     dir = bugdir.tree_root(".")
-    bugs = (dir.list())
     bug = dir.new_bug()
     bug.creator = names.creator()
     bug.severity = "minor"
     bug.status = "open"
     bug.summary = args[0]
     bug.save()
+    bugs = (dir.list())
+    print "Created bug with ID %s" % cmdutil.unique_name(bug, bugs)
 
