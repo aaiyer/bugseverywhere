c51060864bdc774b6ad118f6319ad7c0c4a4815a
Aaron Bentley <aaron.bentley@utoronto.ca>
2005-12-03 17:52:49 -0500

    Changed the shipping configuration file into an example.

--- beweb/beweb/config.py
+++
@@ -1,4 +0,0 @@
-projects = {"be": ("Bugs Everywhere","/home/abentley/be"),
-            "devel": ("PF Devel","/home/abentley/devel"),
-            "release": ("PF Release","/home/abentley/release"),
-}
