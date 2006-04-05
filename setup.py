ba072da930d0f9fd6e62277a9bcd76d6978c919d
Marien Zwart <marienz@gentoo.org>
2006-04-05 02:35:31 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Bugs Everywhere',
+    version='205',
+    description='Bugtracker built on distributed revision control',
+    url='http://panoramicfeedback.com/opensource/',
+    packages=['becommands', 'libbe'],
+    )
