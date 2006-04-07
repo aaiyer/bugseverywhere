255e0c47d97b202e30f4092fac4aac48e3c26bfe
Aaron Bentley <aaron.bentley@utoronto.ca>
2006-04-06 22:24:51 -0400

    Adjust version number in setup.py, so we don't hit 205 prematurely :-)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='Bugs Everywhere',
-    version='205',
+    version='0.0.193',
     description='Bugtracker built on distributed revision control',
     url='http://panoramicfeedback.com/opensource/',
     packages=['becommands', 'libbe'],
