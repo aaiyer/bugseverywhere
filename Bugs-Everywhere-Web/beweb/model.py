3edaf3974b197c89189803b2f99834fd8d735c64
Aaron Bentley <abentley@panoramicfeedback.com>
2006-04-11 18:32:26 -0400

    Used identity for new comments and bugs

--- Bugs-Everywhere-Web/beweb/model.py
+++ Bugs-Everywhere-Web/beweb/model.py
@@ -2,12 +2,14 @@ from sqlobject import *
 from turbogears.database import PackageHub
 # Uncomment the following line if you wish to use Identity and SO_Provider
 from turbogears.identity.soprovider import TG_User, TG_Group, TG_Permission
+from turbogears import identity
 
 hub = PackageHub("beweb")
 __connection__ = hub
 
 def people_map():
-    return dict([(u.userId, u.displayName) for u in TG_User.select()])
+    return dict([(u.userId, u.displayName) for u in TG_User.select() if 
+                "fixbugs" in identity.current.permissions])
 
 # class YourDataClass(SQLObject):
 #     pass
