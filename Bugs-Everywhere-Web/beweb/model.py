868f40a1fce89c338a50290437afc6834fcf094d
Aaron Bentley <abentley@panoramicfeedback.com>
2006-04-11 12:03:28 -0400

    Enable identity tracking

--- Bugs-Everywhere-Web/beweb/model.py
+++ Bugs-Everywhere-Web/beweb/model.py
@@ -1,7 +1,7 @@
 from sqlobject import *
 from turbogears.database import PackageHub
 # Uncomment the following line if you wish to use Identity and SO_Provider
-# from turbogears.identity.soprovider import TG_User, TG_Group, TG_Permission
+from turbogears.identity.soprovider import TG_User, TG_Group, TG_Permission
 
 hub = PackageHub("beweb")
 __connection__ = hub
