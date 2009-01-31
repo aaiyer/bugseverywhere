222237e0745861ec081678f8d23de12982e2bc76
Steve Losh <steve@stevelosh.com>
2009-01-30 22:07:59 -0500

    Minor changes, and added lots of bug reports.

--- cfbe.py
+++ cfbe.py
@@ -4,9 +4,10 @@ import cherrypy
 from libbe import bugdir
 from jinja2 import Environment, FileSystemLoader
 
-bug_root = '/Users/sjl/Desktop/be/.be'
+bug_root = '/Users/sjl/Documents/cherryflavoredbugseverywhere/.be'
 bd = bugdir.BugDir(root=bug_root)
 bd.load_all_bugs()
+repository_name = bd.root.split('/')[-1]
 
 template_root = '/Users/sjl/Documents/cherryflavoredbugseverywhere/templates'
 env = Environment(loader=FileSystemLoader(template_root))
@@ -17,6 +18,7 @@ class WebInterface:
     @cherrypy.expose
     def index(self, status='open', assignee=''):
         bd.load_all_bugs()
+        
         if status == 'open':
             status = ['open', 'assigned', 'test', 'unconfirmed', 'wishlist']
             label = 'All Open Bugs'
@@ -43,7 +45,8 @@ class WebInterface:
             bugs = [bug for bug in bugs if bug.assigned == assignee]
         
         return template.render(bugs=bugs, bd=bd, label=label, 
-                               assignees=possible_assignees)
+                               assignees=possible_assignees,
+                               repository_name=repository_name)
     
 
 config = '/Users/sjl/Documents/cherryflavoredbugseverywhere/cfbe.config'
