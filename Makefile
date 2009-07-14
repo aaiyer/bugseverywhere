859fbf4a83f2806fdd785ca2f312d7dca8b2163e
W. Trevor King <wking@drexel.edu>
2009-07-14 08:13:47 -0400

    Updated GPLv2 to current GPLv2.
    Fixes Ben's bug 00f26f04-9202-4288-8744-b29abc2342d6.
    
    I also tweaked update_copyright.sh to make possible future
    copyright-blurb revision easier.  The new algorithm is greedier,
    overwriting _all_ consecutive comments after a '^# Copyright' line, so
    do
      # Copyright
      #   GPL ... GPL ... GPL
      
      # Your comment here...
    not
      # Copyright
      #   GPL ... GPL ... GPL
      #
      # Your comment here...
    Without the blank line, your comment would get overwritten by the next
    run of update_copyright.sh.
    
    Note that catmutt is ignored by update_copyright.sh because Moritz
    Barsnick has only licensed his grepm code under the GPLv2 (not
    GPLv>=2).  See the initial catmutt commit for details.

--- Makefile
+++ Makefile
@@ -7,11 +7,20 @@
 # Copyright (C) 2008-2009 Ben Finney <benf@cybersource.com.au>
 #                         Chris Ball <cjb@laptop.org>
 #                         W. Trevor King <wking@drexel.edu>
-# This is free software; you may copy, modify and/or distribute this work
-# under the terms of the GNU General Public License, version 2 or later.
-# No warranty expressed or implied. See the file COPYING for details.
-
-# Makefile for Bugs Everywhere project
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 SHELL = /bin/bash
 PATH = /usr/bin:/bin
--- libbe/comment.py
+++ libbe/comment.py
@@ -3,20 +3,19 @@
 #                         Thomas Habets <thomas@habets.pp.se>
 #                         W. Trevor King <wking@drexel.edu>
 #
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#    MA 02110-1301, USA
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 import base64
 import os
 import os.path
