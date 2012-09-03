e63a74393b376753a298231b7917faff97a38704
W. Trevor King <wking@tremily.us>
2012-09-03 08:08:20 -0400

    misc:completion:be.bash: use tabs to indent shell script.

--- misc/completion/be.bash
+++ misc/completion/be.bash
@@ -16,24 +16,24 @@
 #       to print a list of available completions at that point
 _be()
 {
-    local cur prev opts
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    
-    if [ $COMP_CWORD -eq 1 ]; then
-	# no command yet, show all commands
-	COMPREPLY=( $( compgen -W "$(be --complete)" -- $cur ) )
-    else
-	# remove the first word (should be "be") for security reasons
-	unset COMP_WORDS[0]
-	# remove the current word and all later words, because they
-	# are not needed for completion.
-	for i in `seq $COMP_CWORD ${#COMP_WORDS[@]}`; do
-	    unset COMP_WORDS[$i];
-	done
-	COMPREPLY=( $( IFS=$'\n' compgen -W "$(be "${COMP_WORDS[@]}" --complete $cur)" -- $cur ) )
-    fi
+	local cur prev opts
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+	if [ $COMP_CWORD -eq 1 ]; then
+		# no command yet, show all commands
+		COMPREPLY=( $( compgen -W "$(be --complete)" -- $cur ) )
+	else
+		# remove the first word (should be "be") for security reasons
+		unset COMP_WORDS[0]
+		# remove the current word and all later words, because they
+		# are not needed for completion.
+		for i in `seq $COMP_CWORD ${#COMP_WORDS[@]}`; do
+			unset COMP_WORDS[$i];
+		done
+		COMPREPLY=( $( IFS=$'\n' compgen -W "$(be "${COMP_WORDS[@]}" --complete $cur)" -- $cur ) )
+	fi
 }
 
 complete -F _be be
